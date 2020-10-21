8dcf997431802787be9f20c5abb9563f8a64a05c
Igor Ionasku <ionaigor10@gmail.com>
2020-10-21 19:00:25 +0300

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -30,8 +30,8 @@ function destroyBoxes() {
 }
 
 function randColor() {
-  var r = Math.floor(Math.random() * 256),
-    g = Math.floor(Math.random() * 256),
-    b = Math.floor(Math.random() * 256);
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
   return "#" + r.toString(16) + g.toString(16) + b.toString(16);
 }
